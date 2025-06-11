b10c939cec6203110996ccdb6c5cdf80b73c2d1e
BotSolo <116232044+BotSolo621@users.noreply.github.com>
2025-06-11 23:58:04 +1200

    its jover

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,3 @@
 const bgDiv = document.getElementById("bg-cycle");
-const randomIndex = Math.floor(Math.random() * 4) + 1;
+const randomIndex = Math.floor(Math.random() * 2) + 1;
 bgDiv.style.backgroundImage = `url('/src/assets/Backgrouds/${randomIndex}.png')`;
