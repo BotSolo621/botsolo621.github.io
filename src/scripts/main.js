3e5a763d1d0c11dd86f28652ec0781bd0b003307
BotSolo <116232044+BotSolo621@users.noreply.github.com>
2025-06-12 23:12:04 +1200

    prettified codes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,9 @@ const randomIndex = Math.floor(Math.random() * 2) + 1;
 bgDiv.style.backgroundImage = `url('/src/assets/backgrouds/${randomIndex}.png')`;
 
 function scrollToAbout() {
-    const section = document.getElementById("aboutSection");
-    const yOffset = -40; // Adjust offset if needed
-    const y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  const section = document.getElementById("aboutSection");
+  const yOffset = -40; // Adjust offset if needed
+  const y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
-    window.scrollTo({ top: y, behavior: 'smooth' })}
+  window.scrollTo({ top: y, behavior: "smooth" });
+}
