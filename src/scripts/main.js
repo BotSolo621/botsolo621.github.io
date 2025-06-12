cd0c0962320c1bb6f61740e49ecdd590105bdb5d
BotSolo <116232044+BotSolo621@users.noreply.github.com>
2025-06-12 16:46:56 +1200

    not finished
    finish the scroll animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 const bgDiv = document.getElementById("bg-cycle");
 const randomIndex = Math.floor(Math.random() * 2) + 1;
-bgDiv.style.backgroundImage = `url('/src/assets/Backgrouds/${randomIndex}.png')`;
+bgDiv.style.backgroundImage = `url('/src/assets/backgrouds/${randomIndex}.png')`;
+
+function scrollToAbout() {
+    const section = document.getElementById("aboutSection");
+    const yOffset = -40; // Adjust offset if needed
+    const y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+    window.scrollTo({ top: y, behavior: 'smooth' })}
